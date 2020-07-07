213a63f662bb01bd3abfdf2c7087ef6d999d8ac3
luiz marcello <luizmarcello.vmo@hotmail.com>
2020-07-07 09:49:37 -0300

    07.07.2020_09:49hs

--- XF_ListView/XF_ListView/XF_ListView/ListViewPage2.xaml.cs
+++ XF_ListView/XF_ListView/XF_ListView/ListViewPage2.xaml.cs
@@ -9,7 +9,6 @@ using XF_ListView.Models;
 
 namespace XF_ListView
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListViewPage2 : ContentPage
     {
         public ListViewPage2()
@@ -18,9 +17,20 @@ namespace XF_ListView
 
             listview.ItemsSource = new List<Time>
             {
-                new Time {Nome="Palmeiras", Pontos=69,
-                          Imagem="http://macoratti.net/Imagens/times/palmeiras64.png" }
+              new Time { Nome="Palmeiras", Pontos=69,
+                    Imagem ="http://macoratti.net/Imagens/times/palmeiras64.png" },
+              new Time { Nome="Flamengo", Pontos=67,
+                    Imagem ="http://macoratti.net/Imagens/times/flamengo64.jpg" },
+              new Time { Nome="Santos", Pontos=64,
+                    Imagem ="http://macoratti.net/Imagens/times/santos64.jpg" },
+              new Time { Nome="Atletico", Pontos=62,
+                    Imagem ="http://macoratti.net/Imagens/times/atletico64.jpg" },
+              new Time { Nome="Botafogo", Pontos=59,
+                    Imagem ="http://macoratti.net/Imagens/times/botafogo64.jpg" }
             };
         }
     }
 }
+
+
+
