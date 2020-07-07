426a36036cc40bcfb0413323e425c50e7d72f850
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-07-07 01:07:21 -0300

    07.07.2020_01:07hs

---
+++ XF_ListView/XF_ListView/XF_ListView/ListViewPage1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_ListView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage1 : ContentPage
+    {
+        public ListViewPage1()
+        {
+            InitializeComponent();
+
+            List<String> itens = new List<string>()
+            {
+                "Palmeiras","Flamengo","Santos","Atletico","Botafogo"
+            };
+
+            listview.ItemsSource = itens;
+
+        }
+
+        private void listview_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            DisplayAlert("Item Selecionado: ", e.SelectedItem.ToString(), "OK");
+        }
+    }
+}
